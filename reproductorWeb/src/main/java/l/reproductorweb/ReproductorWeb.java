17574dfe96ad91cea295403cec6b29b00a30c18c
GaroAngelxs <133459064+GaroAngelxs@users.noreply.github.com>
2024-01-31 18:51:07 -0800

    Prototipo

---
+++ reproductorWeb/src/main/java/l/reproductorweb/ReproductorWeb.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package l.reproductorweb;
+
+/**
+ *
+ * @author PC
+ */
+public class ReproductorWeb {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
